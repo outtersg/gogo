7185dca1434d236932fe142fd978c9bea0613e7c
Guillaume Outters <guillaume-scripts@outters.eu>
2022-08-19 00:45:01 +0200

    tests/multistepwait: viable
    # We used a Then instead of a Start, thus our parallel tasks were serialized after the latest task.
    # sleeps were not adjusted (let concurrent tasks appear in unpredicatable order).
    = gogogo(), so that asynctasks start immediately (this is a known limitation of gogo, fow now).
    + Clearer output (with both start- and end-trace, instead of just the start.
    
    darcs-hash:63640a54dac6fe008195e20f77abffa19f54dc04

--- tests/multistepwait.test.sh
+++ tests/multistepwait.test.sh
@@ -3,21 +3,34 @@
 
 #- Test dynamic addition of children to the queue ------------------------------
 
+handlerange()
+{
+	echo "Handling range $1 - $2"
+	sleep 2
+	echo "Finished range $1 - $2"
+}
+
 dynl()
 {
 	local id
 	{
 		# Subprocess that spits tasks we will have to launch.
 		echo 1 1000
-		sleep 1
+		sleep 2 # To be sure the following subtasks do not occur before the ending monitors.
 		echo 1001 2000
 		sleep 1
 		echo 2001 2586
 	} | while read from to
 	do
-		Then dync: echo "Handling range $from - $to"
+		Start dync: handlerange $from $to
 	done
 }
-Then dynl: dynl
-After dync~ echo "After first dyn child" # We haven't wait for dynl, so all dyn children are not instanciated: dync~ only evaluates to the first one.
+
+# Launch in gogogo mode: it allows tasks to run as soon as they are declared, while "inline mode" waits for all level-0 tasks to have been scheduled before launching the first one.
+gogogo()
+{
+	Start dynl: dynl
+	sleep 1
+After dync~ echo "After first dyn child" # We haven't wait for dynl, so all dyn children are not instanciated: dync~ only evaluates to the first one (or even no task, because the Then + Then to reach the first dync may be slower than the After we're in.
 After dynl,dync~ echo "After all dyn children" # This one first waits for dynl to have completed, so dync~ has all children.
+}
