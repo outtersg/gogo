64cf960e110c4da80a53c2d1dabfe062fce2a77b
Guillaume Outters <guillaume-scripts@outters.eu>
2022-08-20 00:35:20 +0200

    test.sh
    + Tester and comparator of results.
    
    darcs-hash:e780ae489e572f4e493aa31153f1cf11610238d4

---
+++ tests/test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+fres=/tmp/temp.$$.gogo.test
+
+for f in "$@"
+do
+	printf "=== %s:\\t" "$f"
+	../gogo.sh "$f" > "$fres"
+	[ $? -eq 0 ] || { echo "[31mcrashed[0m" ; continue ; }
+	ftest="`echo "$f" | sed -e 's/\.test\.sh/.res/g'`"
+	diff -quw "$fres" "$ftest" || { echo "[31mfailed (unexpected result)[0m" ; diff -uw "$ftest" "$fres" ; continue ; }
+	echo "[32mOK[0m"
+	rm "$fres"
+done
