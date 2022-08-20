98df2f1cd308629859f2fd9ffddd10520e59244a
Guillaume Outters <guillaume-scripts@outters.eu>
2022-08-21 00:52:45 +0200

    tests/multistepwait: tests / in prereqs
    = Test /.
    = More precise timings (for better results predictability).
    - Comment on expected timings.
    # Waiting for dynl,dync~ was unuseful: as dync is a child of dynl it was always implicitely wait for.
    = slowrange(), to validate that a wait for child is handled differently than an asynchronous one.
    
    darcs-hash:21d80937bfa4af9eb1cff353890e7671deb1d13f

--- tests/multistepwait.res
+++ tests/multistepwait.res
@@ -1,8 +1,12 @@
 Handling range 1 - 1000
-Handling range 1001 - 2000
 Finished range 1 - 1000
-Handling range 2001 - 2586
 After first dyn child
+Handling range 1001 - 2000
+Slowly finished range 1 - 1000
+Handling range 2001 - 2586
 Finished range 1001 - 2000
+Slowly finished range 1001 - 2000
 Finished range 2001 - 2586
-After all dyn children
+After all dynl 'dync' children
+Slowly finished range 2001 - 2586
+After all dynl children
--- tests/multistepwait.test.sh
+++ tests/multistepwait.test.sh
@@ -3,11 +3,29 @@
 
 #- Test dynamic addition of children to the queue ------------------------------
 
+# Expected timings:
+# 0.0 Start dynl, start dync0, start dyns0
+# 0.5 Start monitors
+# 0.8 Finish dync0, "after 1st dync"
+# 1.0 Start dync1, start dyns1
+# 1.2 Finish dyns0
+# 1.6 Start dync2, start dyns2
+# 1.8 Finish dync1
+# 2.2 Finish dyns1
+# 2.4 Finish dync2, "after all dync"
+# 2.8 Finish dyns2
+
 handlerange()
 {
 	echo "Handling range $1 - $2"
-	sleep 2
+	sleep 0.8
 	echo "Finished range $1 - $2"
+}
+
+slowrange()
+{
+	sleep 1.2
+	echo "Slowly finished range $1 - $2"
 }
 
 dynl()
@@ -16,13 +34,14 @@ dynl()
 	{
 		# Subprocess that spits tasks we will have to launch.
 		echo 1 1000
-		sleep 2 # To be sure the following subtasks do not occur before the ending monitors.
+		sleep 1 # To be sure the following subtasks do not occur (and get detected) before the ending monitors.
 		echo 1001 2000
-		sleep 1
+		sleep 0.6
 		echo 2001 2586
 	} | while read from to
 	do
 		Start dync: handlerange $from $to
+		Start dyns: slowrange $from $to
 	done
 }
 
@@ -30,7 +49,8 @@ dynl()
 gogogo()
 {
 	Start dynl: dynl
-	sleep 1
+	sleep 0.5
 After dync~ echo "After first dyn child" # We haven't wait for dynl, so all dyn children are not instanciated: dync~ only evaluates to the first one (or even no task, because the Then + Then to reach the first dync may be slower than the After we're in.
-After dynl,dync~ echo "After all dyn children" # This one first waits for dynl to have completed, so dync~ has all children.
+	After dynl echo "After all dynl children" # dynl and all of its children.
+	After dynl-/dync~ echo "After all dynl 'dync' children" # This one first waits for dynl itself to have completed, and then only, evaluates dync~ (so it knows the 3 of them); but it does not wait for dyns ones.
 }
