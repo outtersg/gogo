7e8dc70b9fe9fc460164602d65c9908b3832c4ee
Guillaume Outters <guillaume-scripts@outters.eu>
2025-10-25 15:43:10 +0200

    bug: test.sh: diff syntax error
    # diff -quw (instead of diff -q || diff -uw) does not work.
    
    darcs-hash:9d206fc769a9524cbd429a168c1427442e64d8bc

--- tests/test.sh
+++ tests/test.sh
@@ -8,7 +8,7 @@ do
 	../gogo.sh "$f" > "$fres"
 	[ $? -eq 0 ] || { echo "[31mcrashed[0m" ; continue ; }
 	ftest="`echo "$f" | sed -e 's/\.test\.sh/.res/g'`"
-	diff -quw "$fres" "$ftest" || { echo "[31mfailed (unexpected result)[0m" ; diff -uw "$ftest" "$fres" ; continue ; }
+	diff -q "$fres" "$ftest" || { echo "[31mfailed (unexpected result)[0m" ; diff -uw "$ftest" "$fres" ; continue ; }
 	echo "[32mOK[0m"
 	rm "$fres"
 done
